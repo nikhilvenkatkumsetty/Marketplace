cec2334a1d1f41e8f321d0f003d62ba0661bb86e
nikhilvenkatkumsetty <43575864+nikhilvenkatkumsetty@users.noreply.github.com>
2020-05-17 20:45:36 +0530

    Add files via upload

---
+++ marketplace-front/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "Rs.0 to Rs.500",
+        array: [0, 500]
+    },
+    {
+        _id: 2,
+        name: "Rs.500 to Rs.1000",
+        array: [500, 1000]
+    },
+    {
+        _id: 3,
+        name: "Rs.1000 to Rs.5000",
+        array: [1000, 5000]
+    },
+    {
+        _id: 4,
+        name: "Rs.5000 to Rs.10000",
+        array: [5000, 10000]
+    },
+    {
+        _id: 5,
+        name: "More than Rs.10000",
+        array: [10000, 99000]
+    }
+];
