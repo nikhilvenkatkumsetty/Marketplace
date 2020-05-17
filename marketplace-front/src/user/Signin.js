1bc04c25d3c66832addf258b7f3a6e239bc86109
nikhilvenkatkumsetty <43575864+nikhilvenkatkumsetty@users.noreply.github.com>
2020-05-17 20:53:13 +0530

    Update Signin.js

--- marketplace-front/src/user/Signin.js
+++ marketplace-front/src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
     const [values, setValues] = useState({
-        email: "nikhil26@gmail.com",
-        password: "qwer1234",
+        email: "",
+        password: "",
         error: "",
         loading: false,
         redirectToReferrer: false
